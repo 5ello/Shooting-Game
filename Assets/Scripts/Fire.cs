e81582a3ea60b73a11100853a669b288d72f412a
5ello <khalil.m.kh@hotmail.com>
2017-06-05 13:33:14 +0400

    First commit

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+	public int bulletsPerMag = 30;
+	public int bulletsLeft = 30;
+	public float fireRate = 10f;
+
+	public Rigidbody rb;
+	public GameObject firePoint;
+	private int bulletSpeed = 100;	
+
+	private float FireDelay;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		if(Input.GetButton("Fire1")){
+			
+			//if(bulletsLeft > 0)
+				FireBullet();
+		}
+		
+		if(FireDelay < fireRate)
+			FireDelay += Time.deltaTime;
+	}
+
+	void FireBullet(){
+
+
+		if(FireDelay < fireRate)
+			return;
+
+ 		FireDelay = 0.0f;
+		
+		bulletsLeft--;
+		GetComponent<AudioSource>().Play();
+		Rigidbody clone;
+		clone = Instantiate(rb, firePoint.transform.position, firePoint.transform.rotation) as Rigidbody;
+		clone.velocity = transform.TransformDirection(Vector3.forward * bulletSpeed);
+
+	}
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+	public Transform target;
+	private bool isWalking = true;
+	private bool isAttacking = false;
+	private bool isDead = false;
+	
+	private int hits = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Bullet"){
+
+			hits++;
+
+			Debug.Log("HIT");
+			if(hits == 3){
+				Destroy(gameObject);
+				Debug.Log("HIT: " + other.gameObject.name);
+				//Die;
+			}
+
+			Destroy(other.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(isWalking == true){
+			transform.Translate(Vector3.forward * Time.deltaTime);
+			transform.LookAt(target);
+		}
+		
+	}
+}
